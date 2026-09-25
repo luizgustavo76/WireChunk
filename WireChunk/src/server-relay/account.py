6e8a1b1ab2fec99aaa3a95f6d7c159d0e9101f28
luizgustavo76 <luizsgustavo76@gmail.com>
2026-09-25 17:57:09 -0300

    adição de rota de login

--- WireChunk/src/server-relay/account.py
+++ WireChunk/src/server-relay/account.py
@@ -21,7 +21,7 @@ def register():
     username = data.get("username")
     password = data.get("password")
     if None in [username, password]:
-        return jsonify({"status":"username os password is missing"}),400
+        return jsonify({"status":"username or password is missing"}),400
     conn = get_db()
     cur = conn.cursor()
     cur.execute("SELECT username FROM account WHERE username = ?",(username,))
@@ -29,8 +29,25 @@ def register():
         conn.close()
         return jsonify({"status":"username already exists"}),401
     password_hash = generate_password_hash(password)
-    cur.execute("INSERT INTO accounts (username, password) VALUES(?, ?)",(username, password_hash))
+    cur.execute("INSERT INTO account (username, password) VALUES(?, ?)",(username, password_hash))
     conn.commit()
     conn.close()
-
+    return jsonify({"status":"the account has been created"}),201
+@account_bp.route("/login", methods=["POST"])
+def login():
+    data = request.get_json()
+    username = data.get("username")
+    password = data.get("password")
+    if None in [username, password]:
+        return jsonify({"status":"usernane or password is missing"}),400
+    conn = get_db()
+    cur = conn.cursor()
+    cur.execute("SELECT username, password FROM account WHERE username = ?",(username,))
+    result = cur.fetchone()
+    password_hash = result[1]
+    conn.close()
+    if check_password_hash(password_hash, password):
+        return jsonify({"status":"login has sucessful"}),200
+    else:
+        return jsonify({"status":"username or password is incorrect"}),401
     
