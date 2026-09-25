d055b565de91ff93f78e32965b60bc2689f18d72
luizgustavo76 <luizsgustavo76@gmail.com>
2026-09-25 17:35:40 -0300

    rota de register feita

--- WireChunk/src/server-relay/account.py
+++ WireChunk/src/server-relay/account.py
@@ -1,5 +1,8 @@
 from flask import Blueprint, jsonify, request
 import sqlite3
+from werkzeug.security import generate_password_hash
+from werkzeug.security import check_password_hash
+account_bp = Blueprint("account_bp", __name__)
 def get_db():
     conn = sqlite3.connect("accounts.db")
     return conn
@@ -12,3 +15,22 @@ def create_db():
     conn.commit()
     conn.close()
 create_db()
+@account_bp.route("/register", methods=["post"])
+def register():
+    data = request.get_json()
+    username = data.get("username")
+    password = data.get("password")
+    if None in [username, password]:
+        return jsonify({"status":"username os password is missing"}),400
+    conn = get_db()
+    cur = conn.cursor()
+    cur.execute("SELECT username FROM account WHERE username = ?",(username,))
+    if cur.fetchone():
+        conn.close()
+        return jsonify({"status":"username already exists"}),401
+    password_hash = generate_password_hash(password)
+    cur.execute("INSERT INTO accounts (username, password) VALUES(?, ?)",(username, password_hash))
+    conn.commit()
+    conn.close()
+
+    
--- WireChunk/src/server-relay/main.py
+++ WireChunk/src/server-relay/main.py
@@ -1,4 +1,6 @@
 from flask import Flask, Blueprint
+from account import account_bp
 app = Flask(__name__)
+app.register_blueprint(account_bp)
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True, port=6000)
